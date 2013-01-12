2aeae468003c16e1cf58a25dca240d796d2cdd95
Dennis Cornwell <cornwe19@arctic.cse.msu.edu>
2013-01-12 17:25:48 -0500

    Making a matlab file to generate a random plot for HW1

---
+++ HW1/hw1.m
@@ -0,0 +1,27 @@
+% Feature Length
+LFW1 = abs( random( 'normal', 0.250, 0.07, 10, 1 ) )
+SFW1 = abs( random( 'normal', 0.125, 0.07, 10, 1 ) )
+SMS1 = abs( random( 'normal', 1.000, 0.07, 10, 1 ) )
+MS1  = abs( random( 'normal', 1.250, 0.07, 10, 1 ) )
+HN1  = abs( random( 'normal', 0.190, 0.07, 10, 1 ) )
+
+% Feature Thickness
+LFW2 = abs( random( 'normal', 0.062, 0.05, 10, 1 ) )
+SFW2 = abs( random( 'normal', 0.030, 0.05, 10, 1 ) )
+SMS2 = abs( random( 'normal', 0.083, 0.05, 10, 1 ) )
+MS2  = abs( random( 'normal', 0.092, 0.05, 10, 1 ) )
+HN2  = abs( random( 'normal', 0.750, 0.05, 10, 1 ) )
+
+M    = [LFW1 LFW2; SFW1 SFW2; SMS1 SMS2; MS1 MS2; HN1 HN2]
+
+figure(1)
+hold on;
+plot( LFW1, LFW2, '+' );
+plot( SFW1, SFW2, 'o' );
+plot( SMS1, SMS2, '*' );
+plot( MS1, MS2, 'x' );
+plot( HN1, HN2, '.' );
+hold off;
+legend( 'Flat Washer 0.25', 'Flat Washer 0.125', 'Sheet Metal Screw', 'Machine Screw', 'Hex Nut' );
+xlabel( 'Length' );
+ylabel( 'Thickness' );
